0a9d31b3606982c75805a892062b9c1858d79c24
Мария Мелентьева <masha696nik@gmail.com>
2025-07-19 14:58:18 +0500

    chore: update file path

--- js/data.js
+++ js/data.js
@@ -1,267 +1,267 @@
 var data = [
     {
         name: "advanced_scroll",
-        img: "./img/advanced_scroll.png",
+        img: "./img/cards/advanced_scroll.png",
         id: 1,
     },
     {
         name: "advanced_spellbook",
-        img: "./img/advanced_spellbook.png",
+        img: "./img/cards/advanced_spellbook.png",
         id: 2
     },
     {
         name: "antidote",
-        img: "./img/antidote.png",
+        img: "./img/cards/antidote.png",
         id: 3
     },
     {
         name: "apple",
-        img: "./img/apple.png",
+        img: "./img/cards/apple.png",
         id: 4
     },
     {
         name: "bogling",
-        img: "./img/bogling.png",
+        img: "./img/cards/bogling.png",
         id: 5
     },
     {
         name: "bronze_ingot",
-        img: "./img/bronze_ingot.png",
+        img: "./img/cards/bronze_ingot.png",
         id: 6
     },
     {
         name: "cockatrice",
-        img: "./img/cockatrice.png",
+        img: "./img/cards/cockatrice.png",
         id: 7
     },
     {
         name: "colorful_coral",
-        img: "./img/colorful_coral.png",
+        img: "./img/cards/colorful_coral.png",
         id: 8
     },
     {
         name: "darkness_crystal",
-        img: "./img/darkness_crystal.png",
+        img: "./img/cards/darkness_crystal.png",
         id: 9
     },
     {
         name: "darkness_potion",
-        img: "./img/darkness_potion.png",
+        img: "./img/cards/darkness_potion.png",
         id: 10
     },
     {
         name: "earth_crystal",
-        img: "./img/earth_crystal.png",
+        img: "./img/cards/earth_crystal.png",
         id: 11
     },
     {
         name: "earth_scroll",
-        img: "./img/earth_scroll.png",
+        img: "./img/cards/earth_scroll.png",
         id: 12
     },
     {
         name: "enchanted_obsidian",
-        img: "./img/enchanted_obsidian.png",
+        img: "./img/cards/enchanted_obsidian.png",
         id: 13
     },
     {
         name: "fabric",
-        img: "./img/fabric.png",
+        img: "./img/cards/fabric.png",
         id: 14
     },
     {
         name: "fairy_pollen",
-        img: "./img/fairy_pollen.png",
+        img: "./img/cards/fairy_pollen.png",
         id: 15
     },
     {
         name: "fire_crystal",
-        img: "./img/fire_crystal.png",
+        img: "./img/cards/fire_crystal.png",
         id: 16
     },
     {
         name: "fire_potion",
-        img: "./img/fire_potion.png",
+        img: "./img/cards/fire_potion.png",
         id: 17
     },
     {
         name: "fire_slime",
-        img: "./img/fire_slime.png",
+        img: "./img/cards/fire_slime.png",
         id: 18
     },
     {
         name: "fire_spellbook",
-        img: "./img/fire_spellbook.png",
+        img: "./img/cards/fire_spellbook.png",
         id: 19
     },
     {
         name: "glistening_fish",
-        img: "./img/glistening_fish.png",
+        img: "./img/cards/glistening_fish.png",
         id: 20
     },
     {
         name: "glowshroom",
-        img: "./img/glowshroom.png",
+        img: "./img/cards/glowshroom.png",
         id: 21
     },
     {
         name: "gold_ingot",
-        img: "./img/gold_ingot.png",
+        img: "./img/cards/gold_ingot.png",
         id: 22
     },
     {
         name: "golden_apple",
-        img: "./img/golden_apple.png",
+        img: "./img/cards/golden_apple.png",
         id: 23
     },
     {
         name: "gorgon",
-        img: "./img/gorgon.png",
+        img: "./img/cards/gorgon.png",
         id: 24
     },
     {
         name: "griffin_wing",
-        img: "./img/griffin_wing.png",
+        img: "./img/cards/griffin_wing.png",
         id: 25
     },
     {
         name: "harpy",
-        img: "./img/harpy.png",
+        img: "./img/cards/harpy.png",
         id: 26
     },
     {
         name: "health_potion",
-        img: "./img/health_potion.png",
+        img: "./img/cards/health_potion.png",
         id: 27
     },
     {
         name: "holy_book",
-        img: "./img/holy_book.png",
+        img: "./img/cards/holy_book.png",
         id: 28
     },
     {
         name: "ice_crystal",
-        img: "./img/ice_crystal.png",
+        img: "./img/cards/ice_crystal.png",
         id: 29
     },
     {
         name: "ice_scroll",
-        img: "./img/ice_scroll.png",
+        img: "./img/cards/ice_scroll.png",
         id: 30
     },
     {
         name: "jellyfish",
-        img: "./img/jellyfish.png",
+        img: "./img/cards/jellyfish.png",
         id: 31
     },
     {
         name: "light_crystal",
-        img: "./img/light_crystal.png",
+        img: "./img/cards/light_crystal.png",
         id: 32
     },
     {
         name: "light_potion",
-        img: "./img/light_potion.png",
+        img: "./img/cards/light_potion.png",
         id: 33
     },
     {
         name: "luck_potion",
-        img: "./img/luck_potion.png",
+        img: "./img/cards/luck_potion.png",
         id: 34
     },
     {
         name: "magestone",
-        img: "./img/magestone.png",
+        img: "./img/cards/magestone.png",
         id: 35
     },
     {
         name: "magic_potion",
-        img: "./img/magic_potion.png",
+        img: "./img/cards/magic_potion.png",
         id: 36
     },
     {
         name: "mana_potion",
-        img: "./img/mana_potion.png",
+        img: "./img/cards/mana_potion.png",
         id: 37
     },
     {
         name: "medicinal_herb",
-        img: "./img/medicinal_herb.png",
+        img: "./img/cards/medicinal_herb.png",
         id: 38
     },
     {
         name: "mimic",
-        img: "./img/mimic.png",
+        img: "./img/cards/mimic.png",
         id: 39
     },
     {
         name: "moonlit_pearl",
-        img: "./img/moonlit_pearl.png",
+        img: "./img/cards/moonlit_pearl.png",
         id: 40
     },
     {
         name: "nature_slime",
-        img: "./img/nature_slime.png",
+        img: "./img/cards/nature_slime.png",
         id: 41
     },
     {
         name: "ogre_toenail",
-        img: "./img/ogre_toenail.png",
+        img: "./img/cards/ogre_toenail.png",
         id: 42
     },
     {
         name: "old_book",
-        img: "./img/old_book.png",
+        img: "./img/cards/old_book.png",
         id: 43
     },
     {
         name: "parchment",
-        img: "./img/parchment.png",
+        img: "./img/cards/parchment.png",
         id: 44
     },
     {
         name: "regeneration_potion",
-        img: "./img/regeneration_potion.png",
+        img: "./img/cards/regeneration_potion.png",
         id: 45
     },
     {
         name: "resurrection_potion",
-        img: "./img/resurrection_potion.png",
+        img: "./img/cards/resurrection_potion.png",
         id: 46
     },
     {
         name: "royal_book",
-        img: "./img/royal_book.png",
+        img: "./img/cards/royal_book.png",
         id: 47
     },
     {
         name: "salamander",
-        img: "./img/salamander.png",
+        img: "./img/cards/salamander.png",
         id: 48
     },
     {
         name: "shadow_root",
-        img: "./img/shadow_root.png",
+        img: "./img/cards/shadow_root.png",
         id: 49
     },
     {
         name: "silver_ingot",
-        img: "./img/silver_ingot.png",
+        img: "./img/cards/silver_ingot.png",
         id: 50
     },
     {
         name: "tengu",
-        img: "./img/tengu.png",
+        img: "./img/cards/tengu.png",
         id: 51
     },
     {
         name: "thunder_crystal",
-        img: "./img/thunder_crystal.png",
+        img: "./img/cards/thunder_crystal.png",
         id: 52
     },
     {
         name: "will-o-wisp",
-        img: "./img/will-o-wisp.png",
+        img: "./img/cards/will-o-wisp.png",
         id: 53
     },
     
